435226c2ade47e12424f46430bf65a42f91b67bb
Chris Andersson <chrand2017@gmail.com>
2018-03-01 09:34:13 +0100

    GarageIsFullException added

--- src/model/Garage.java
+++ src/model/Garage.java
@@ -1,5 +1,46 @@
 package model;
 
-public class Garage {
+import java.util.ArrayList;
 
+public class Garage {
+	private int maxCapacity;
+	private int noOfVehicles;
+	private String name;
+	private ArrayList<Vehicle> vehicles;
+	
+	public Garage(String name, int capacity) {
+		this.name = name;
+		maxCapacity = capacity;
+		noOfVehicles = 0;
+		vehicles = new ArrayList<Vehicle>();
+	}
+	
+	public void setMaxCapacity(int capacity) {
+		maxCapacity = capacity;
+	}
+	
+	public int getMaxCapacity() {
+		return maxCapacity;
+	}
+	
+	public int getNoOfVehicles() {
+		return noOfVehicles;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void addVehicle(Vehicle newVehicle) throws GarageIsFullException {
+		if(noOfVehicles == maxCapacity) {
+			throw new GarageIsFullException();
+		}
+		vehicles.add(newVehicle);
+		noOfVehicles++;
+	}
+	
+	public void removeVehicle(Vehicle vehicle) {
+		vehicles.remove(vehicle);
+		noOfVehicles = vehicles.size();
+	}
 }
---
+++ src/model/GarageIsFullException.java
@@ -0,0 +1,5 @@
+package model;
+
+public class GarageIsFullException extends Exception {
+
+}
